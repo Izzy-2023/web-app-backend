c2074e5c0664789758e201cddba08e6608fe61c8
izzy-2023 <izzyzinxhirija@gmail.com>
2024-08-28 23:08:34 -0400

    fixed db connection

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
+require('./db'); // Import db.js to establish the MongoDB connection
 
 const app = express();
+
+// middleware
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+
+//routes
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
